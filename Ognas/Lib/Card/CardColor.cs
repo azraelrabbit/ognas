4750df87fb46a5ef01ad3973a8ce41a14babc1ff
bmibbb <bmibbb@94d712ca-dd8d-d52f-e9a3-d49e0f32b722>
2011-01-07 02:40:29 +0000

    add card

---
+++ Ognas/Lib/Card/CardColor.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Ognas.Server.Card
+{
+    public enum CardColor
+    {
+        // Changed by Wang, Yajing , 2011-01-07 10:33
+
+        // 黑桃
+        Spade = 1,
+        // 红桃
+        Heart = 2,
+        // 梅花
+        Club = 3,
+        // 方块
+        Diamond = 4,
+    }
+}
