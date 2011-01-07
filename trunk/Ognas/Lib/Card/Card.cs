d2d9a81c442807f556c8877ae4781f58756473b5
bmibbb <bmibbb@94d712ca-dd8d-d52f-e9a3-d49e0f32b722>
2011-01-07 02:40:29 +0000

    add card

---
+++ trunk/Ognas/Lib/Card/Card.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Ognas.Server.Card
+{
+    public class Card
+    {
+        /// <summary>
+        /// 卡牌名称（杀，闪。。。。）
+        /// </summary>
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 卡牌代码，唯一标示一张卡牌(01,02,1A,1B.....)
+        /// </summary>
+        public string Code
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 卡牌花色（红色，黑色）
+        /// </summary>
+        public CardColor Color
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 卡牌点数（A,2,3,4,5,6,7,8,9,10,J,Q,K）
+        /// </summary>
+        public CardNumber Number
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// 卡牌说明
+        /// </summary>
+        public string Remark
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ trunk/Ognas/Lib/Card/CardNumber.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Ognas.Server.Card
+{
+    public enum CardNumber
+    {
+        A = 1,
+        J = 11,
+        Q = 12,
+        K = 13
+    }
+}
