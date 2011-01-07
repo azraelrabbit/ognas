b1f6eba78146e27b489a535761484356d908898f
bmibbb <bmibbb@94d712ca-dd8d-d52f-e9a3-d49e0f32b722>
2011-01-07 02:10:06 +0000

    add card enum

---
+++ trunk/Ognas/OgnasSrv/Card/CardColor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ognas.Server.Card
+{
+    public enum CardColor
+    {
+        Red = 0,
+        Black = 1
+    }
+}
