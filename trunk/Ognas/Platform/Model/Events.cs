a3f5218127a4ce82ded82e0494d552bed0de43d6
hbwang518g@gmail.com <hbwang518g@gmail.com@94d712ca-dd8d-d52f-e9a3-d49e0f32b722>
2011-01-10 04:18:11 +0000

    Add event class for solution.

---
+++ trunk/Ognas/Platform/Model/Events.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platform.Model
+{
+    public class Events
+    {
+        //public event void InitializePlay;
+    }
+}
