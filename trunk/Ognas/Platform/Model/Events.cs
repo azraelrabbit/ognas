0fab0c0eedf3c0d909f8acf0bf8362c1ae2a5b19
azraelrabbit <azraelrabbit@94d712ca-dd8d-d52f-e9a3-d49e0f32b722>
2011-01-10 07:12:51 +0000

    update for added custome eventargs and game class

--- trunk/Ognas/Platform/Model/Events.cs
+++ trunk/Ognas/Platform/Model/Events.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Platform.Model;
+using Ognas.Server.Cards;
+using Platform.Model.OgnasEventArgs;
 
 namespace Platform.Model
 {
+    public delegate void UserCardUse(object sender, CardUseEventArgs args);
+
     public class Events
     {
         //public event void InitializePlay;
---
+++ trunk/Ognas/Platform/Model/OgnasEventArgs/CardUseEventArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Platform.Model;
+using Ognas.Server.Cards;
+
+namespace Platform.Model.OgnasEventArgs
+{
+    public class CardUseEventArgs : EventArgs
+    {
+        public User sourceUser
+        {
+            get;
+            set;
+        }
+
+        public User targetUser
+        {
+            get;
+            set;
+        }
+
+        public Card card
+        {
+            get;
+            set;
+        }
+
+    }
+}
