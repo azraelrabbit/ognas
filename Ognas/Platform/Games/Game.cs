f3b018d7480cb60f13d3c03f3649306ea95c2d7d
azraelrabbit <azraelrabbit@94d712ca-dd8d-d52f-e9a3-d49e0f32b722>
2011-01-10 07:12:51 +0000

    update for added custome eventargs and game class

---
+++ Ognas/Platform/Games/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Platform.Model;
+using Ognas.Server.Cards;
+
+namespace Platform.Games
+{
+    public class Game : GameBase
+    {
+        public Game()
+        {
+        }
+
+        public Game(List<User> userlist)
+        {
+            this.userList = userlist;
+        }
+    }
+}
---
+++ Ognas/Platform/Games/GameBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Platform.Model;
+using Ognas.Server.Cards;
+
+namespace Platform.Games
+{
+    public class GameBase
+    {
+        User currentTokenUser
+        {
+            get;
+            set;
+        }
+
+        public List<User> userList
+        {
+            get;
+            set;
+        }
+
+        Room currentRoom
+        {
+            get;
+            set;
+        }
+
+        public GameBase()
+        {
+        }
+
+        public GameBase(List<User> userlist)
+        {
+            this.userList = userlist;
+        }
+
+        /// <summary>
+        /// Game Starting
+        /// </summary>
+        public void GameStart()
+        {
+            //TODO: 
+
+            // 初始化游戏全局变量
+
+            // 分派游戏身份
+
+            // 随机设置座位次序
+
+            // 选择武将
+
+            // 发牌
+
+            // 用户令牌移交到第一顺位玩家
+        }
+    }
+}
