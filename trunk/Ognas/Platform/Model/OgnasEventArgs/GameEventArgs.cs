c74b5092bda6fe46c8fdf7c0388a2d1a0123b8fc
azraelrabbit <azraelrabbit@94d712ca-dd8d-d52f-e9a3-d49e0f32b722>
2011-01-10 08:19:40 +0000

    update for added custome eventargs and declare the game start logic

---
+++ trunk/Ognas/Platform/Model/OgnasEventArgs/GameEventArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Platform.Model;
+using Ognas.Server.Cards;
+
+namespace Platform.Model.OgnasEventArgs
+{
+    public class GameEventArgs : EventArgs
+    {
+        public List<Card> cardList
+        {
+            get;
+            set;
+        }
+
+        public List<User> userList
+        {
+            get;
+            set;
+        }
+
+        public string Messages
+        {
+            get;
+            set;
+        }
+
+    }
+}
