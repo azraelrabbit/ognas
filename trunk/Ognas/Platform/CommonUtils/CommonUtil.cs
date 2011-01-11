b8e613faa9248a13e1fc156a7de5ba19d5d1ae89
hbwang518g@gmail.com <hbwang518g@gmail.com@94d712ca-dd8d-d52f-e9a3-d49e0f32b722>
2011-01-11 09:53:51 +0000

    Add CommonUtil file to solution.

---
+++ trunk/Ognas/Platform/CommonUtils/CommonUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platform.CommonUtils
+{
+    public static class CommonUtil
+    {
+        public static string GetOutputException(Exception ex)
+        {
+            return string.Format("Exception Message : {1} {0} Exception StackTrace{2}", Environment.NewLine, ex.Message, ex.StackTrace);
+        }
+
+        public static string GetOutputExceptionWithNewLine(Exception ex)
+        {
+            return string.Format("{0} {1}", Environment.NewLine, GetOutputException(ex));
+        }
+    }
+}
