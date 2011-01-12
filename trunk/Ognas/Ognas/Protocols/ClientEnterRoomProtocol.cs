09726f6a99236bbb39ff362fbf742ead17b32046
hbwang518g@gmail.com <hbwang518g@gmail.com@94d712ca-dd8d-d52f-e9a3-d49e0f32b722>
2011-01-12 09:10:37 +0000

    add client create room protocal to client project.

---
+++ trunk/Ognas/Ognas/Protocols/ClientEnterRoomProtocol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ognas.Lib.Protocols;
+
+namespace Ognas.Client.Protocols
+{
+    public class ClientEnterRoomProtocol : EnterRoomProtocol
+    {
+    }
+}
